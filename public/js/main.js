3aa5d668709cc4ea706f6f7ff7c6614eabe3c4e7
Harshil Rathod <harshilrathod233@gmail.com>
2022-03-30 21:28:50 +0530

    Test commit 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,35 @@
+const { response } = require("express");
+
+const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name');
+const temp = document.getElementById('temp');
+const temp_status = document.getElementById('temp_status');
+
+const getInfo = (event) => {
+    event.preventDefault();
+    let cityVal = cityName.Value;
+
+    if(cityVal=== ""){
+        city_name.innertText = `Please enter your city`;
+
+    } else {
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=d75577fab323b32a76d8a546b780b216`;
+            const response = await fetch(url);
+            const data  = await response.json();
+            const arrData = [Data];
+
+            temp.innerText = arrData[0].main.temp;
+            temp_status.innerText = arrData[0].weather[0].main;
+        }
+        catch{
+            city_name.innerText - 'plz enter correctly';
+            
+        }
+    }
+    
+
+
+}
+submitBtn.addEventListener('click', getInfo);
